f0fe8e070323a5ae41678f73d1a6dfe46e537833
seongkyun <yooss135@naver.com>
2025-04-28 05:33:41 +0900

    feat: requestParameter 출력

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        System.out.println("RequestParamServlet.service");
+
+        System.out.println("[전체 파라미터 조회] - start");
+        req.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + "=" + req.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+        System.out.println("[단일 파라미터 조회]");
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+        System.out.println();
+        String[] usernames = req.getParameterValues("username");
+        for (String s : usernames) {
+            System.out.println("username = " + s);
+        }
+
+    }
+}
