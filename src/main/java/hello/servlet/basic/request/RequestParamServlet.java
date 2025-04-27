ac40d387d1a32e4589f7402901b9eaf9a64a46bf
seongkyun <yooss135@naver.com>
2025-04-28 06:08:35 +0900

    feat: json 데이터 자바 객체로 파싱해서 출력

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
